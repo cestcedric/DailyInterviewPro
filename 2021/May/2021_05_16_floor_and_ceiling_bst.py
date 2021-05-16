9b1f2c74047b46016cd8d2835675092dc9e5310a
Cedric Stark <cedric.stark@outlook.com>
2021-05-16 21:03:51 +0200

    2021-05-16: shorten recursive implementation a lot by taking into account the sorted nature of a BST

--- 2021/May/2021_05_16_floor_and_ceiling_bst.py
+++ 2021/May/2021_05_16_floor_and_ceiling_bst.py
@@ -11,33 +11,21 @@ def findCeilingFloor(root_node, k, floor=None, ceil=None):
 
 
 def findCeilingFloorRec(root_node, k, floor=None, ceil=None):
-    if root_node is None: return None, None
-    if root_node.value <= k:
-        print('Found {}'.format(root_node.value))
+    if root_node is None: return floor, ceil
+
+    # floor: biggest number <= k
+    # if node.value > k => go left, if node.value < k go right
+    # ceil: smallest number >= k
+    # same, just backwards
+    if root_node.value == k: return k, k
+    elif root_node.value < k:
         if floor is None: floor = root_node.value
         else: floor = max(floor, root_node.value)
-    if root_node.value >= k:
+        floor, ceil = findCeilingFloorRec(root_node.right, k, floor, ceil)
+    else:
         if ceil is None: ceil = root_node.value
         else: ceil = min(ceil, root_node.value)
-
-    left = findCeilingFloorRec(root_node.left, k, floor, ceil)
-    right = findCeilingFloorRec(root_node.right, k, floor, ceil)
-    
-    if floor is not None:
-        if left[0] is not None: floor = max(floor, left[0])
-        if right[0] is not None: floor = max(floor, right[0])
-    else:
-        if left[0] is not None and right[0] is not None: floor = max(left[0], right[0])
-        elif left[0] is None: floor = right[0]
-        else: floor = left[0]
-
-    if ceil is not None:
-        if left[1] is not None: ceil = min(ceil, left[1])
-        if right[1] is not None: ceil = min(ceil, right[1])
-    else:
-        if left[1] is not None and right[1] is not None: ceil = min(left[1], right[1])
-        if left[1] is None: ceil = right[1]
-        else: ceil = left[1]
+        floor, ceil = findCeilingFloorRec(root_node.left, k, floor, ceil)
 
     return floor, ceil
 
