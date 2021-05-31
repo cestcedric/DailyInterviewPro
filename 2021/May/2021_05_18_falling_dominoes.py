b5bf5801b457c2f4dffbdec51f2b5f98cb52d23a
Cedric Stark <cedric.stark@outlook.com>
2021-05-31 20:29:57 +0200

    2021-05-28: iterative solution

---
+++ 2021/May/2021_05_18_falling_dominoes.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def pushDominoes(self, dominoes):
+        # O(n^2) time complexity, iterate over whole string for each step
+        # At most all dominoes fall
+        # O(n) space complexity, one list created with same size as dominoes
+        pressure = 'R' in dominoes or 'L' in dominoes
+        length = len(dominoes)
+        if length < 2: return dominoes
+        while pressure:
+            tmp = list(dominoes)
+            pressure = False
+            if dominoes[0] == '.' and dominoes[1] == 'L': tmp[0] = 'L'
+            for i in range(1, length - 1):
+                if dominoes[i] == '.':
+                    right =  dominoes[i-1] == 'R'
+                    left = dominoes[i+1] == 'L'
+                    if left != right: 
+                        tmp[i] = 'R' if right else 'L'
+                        pressure = True
+            if dominoes[length-1] == '.' and dominoes[length-2] == 'R': tmp[length-1] = 'R'
+            dominoes = ''.join(tmp)
+        return dominoes
+
+
+print('Should be \'..RR.LL..RR\': {}'.format(Solution().pushDominoes('..R...L..R.')))
+print('Should be \'RR.L\': {}'.format(Solution().pushDominoes('RR.L')))
+print('Should be \'....\': {}'.format(Solution().pushDominoes('....')))
