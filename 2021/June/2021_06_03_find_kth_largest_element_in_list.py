ce8cc9853f06a5f60f321844989c505d58813fb9
Cedric Stark <cedric.stark@outlook.com>
2021-06-08 00:58:11 +0200

    2021-06-03: solve using heap

--- 2021/June/2021_06_03_find_kth_largest_element_in_list.py
+++ 2021/June/2021_06_03_find_kth_largest_element_in_list.py
@@ -1,7 +1,18 @@
+import heapq
+
 # O(n log(n)) time complexity: sorting takes that time
 # O(1) space complexity when sorting in-place, O(n) otherwise
-def findKthLargest(nums, k):
+def findKthLargestSorted(nums, k):
     return sorted(nums)[-k]
+
+# O(k log(n)) time complexity: O(n) for heapify, O(log(n)) for each heappop (k)
+# O(n) space complexity for heap
+def findKthLargest(nums, k):
+    heap = [ -n for n in nums ]
+    heapq.heapify(heap)
+    for _ in range(k-1):
+        heapq.heappop(heap)
+    return - heapq.heappop(heap)
 
 
 print('Should be 5: {}'.format(findKthLargest([3, 5, 2, 4, 6, 8], 3)))
