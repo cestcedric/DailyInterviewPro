1192ba7b9f47089798465231541579fc0948b841
Cedric Stark <cedric.stark@outlook.com>
2021-06-09 00:08:32 +0200

    2021-06-08: solve with improved average time complexity

--- 2021/June/2021_06_08_queue_using_2_stacks.py
+++ 2021/June/2021_06_08_queue_using_2_stacks.py
@@ -20,7 +20,33 @@ class Queue:
             self.stack1.append(self.stack2.pop())
         return ret
 
-q = Queue()
+
+class QueueEff:
+    def __init__(self):
+        self.size1 = 0
+        self.size2 = 0
+        self.stack1 = []
+        self.stack2 = []
+        
+    # O(1) time complexity
+    def enqueue(self, val):
+        self.stack1.append(val)
+        self.size1 += 1
+
+    # Worst case: O(n) time complexity when moving stack1 to stack2
+    # Average: (1) time complexity, one pop prepares stack2 for the next n pops
+    def dequeue(self):
+        if self.size1 + self.size2 == 0: return None
+        if self.size2 > 0:
+            self.size2 -= 1
+            return self.stack2.pop()
+        for _ in range(self.size1):
+            self.stack2.append(self.stack1.pop())
+        self.size2 = self.size1 - 1
+        self.size1 = 0
+        return self.stack2.pop()
+
+q = QueueEff()
 q.enqueue(1)
 q.enqueue(2)
 q.enqueue(3)
