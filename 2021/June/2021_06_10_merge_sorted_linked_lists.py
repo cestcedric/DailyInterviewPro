d46cb6417e6e2f8b35f0641e9c76dfe50690fa31
Cedric Stark <cedric.stark@outlook.com>
2021-06-10 20:09:46 +0200

    2021-06-10: add heap-based solution

--- 2021/June/2021_06_10_merge_sorted_linked_lists.py
+++ 2021/June/2021_06_10_merge_sorted_linked_lists.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Node(object):
     def __init__(self, val, next=None):
         self.val = val
@@ -11,7 +13,7 @@ class Node(object):
             c = c.next
         return answer
 
-# O(n*l) time complexity: l lists of average length n
+# O(n*l^2) time complexity: l lists of average length n
 # O(1) time complexity, we only work with already present nodes and rearrange
 def merge(lists):
     # check if any list is not None = last element reached
@@ -54,17 +56,38 @@ def merge(lists):
         else: lists[_minIndex] = lists[_minIndex].next
     return merged
 
+# O(n*l) time complexity: look at each node twice (l lists, average length n)
+# O(n*l) space complexity: create new node for each input node
+def mergeHeap(lists):
+    heap = []
+    heapq.heapify(heap)
+    for l in lists:
+        node = l
+        while node is not None: 
+            heapq.heappush(heap, node.val)
+            node = node.next
+
+    if not heap: return None
+
+    merged = Node(heapq.heappop(heap))
+    node = merged
+    while heap:
+        node.next = Node(heapq.heappop(heap))
+        node = node.next
+    return merged
+
+
 
 a = Node(1, Node(3, Node(5)))
 b = Node(2, Node(4, Node(6)))
 c = Node(6, Node(7, Node(8)))
-print('Should be 123456: {}'.format(merge([a, b])))
+print('Should be 123456: {}'.format(mergeHeap([a, b])))
 a = Node(1, Node(3, Node(5)))
 b = Node(2, Node(4, Node(6)))
 c = Node(6, Node(7, Node(8)))
-print('Should be 123456678: {}'.format(merge([a, b, c])))
-print('Should be None: {}'.format(merge([])))
+print('Should be 123456678: {}'.format(mergeHeap([a, b, c])))
+print('Should be None: {}'.format(mergeHeap([])))
 a = Node(1, Node(3, Node(5)))
 b = Node(2, Node(4, Node(6)))
 c = Node(6, Node(7, Node(8)))
-print('Should be 123456: {}'.format(merge([a, None, None, b])))
+print('Should be 123456: {}'.format(mergeHeap([a, None, None, b])))
