bc4e847b1dc880a5c540d6d979fc54b25254d735
Cedric Stark <cedric.stark@outlook.com>
2021-07-01 13:08:44 +0200

    2021-06-30: solve with heap

--- 2021/June/2021_06_30_room_scheduling.py
+++ 2021/June/2021_06_30_room_scheduling.py
@@ -1,3 +1,5 @@
+import heapq
+
 # O(n^2) time complexity: if all events at the same time, we check k - 1 rooms for event k
 # O(n) space complexity: at most n rooms
 def scheduling(events: list) -> int:
@@ -18,6 +20,24 @@ def scheduling(events: list) -> int:
     return r
             
 
-print('Should be 2:', scheduling([[0, 30],[5, 10],[15, 20]]))
-print('Should be 2:', scheduling([(30, 75), (0, 50), (60, 150)]))
-print('Should be 1:', scheduling([[7,10],[2,4]]))
+# O(n * log(n)) time complexity: heappush in O(log(n)), happens O(n) times
+# O(n) space complexity: heap with at most n elements
+def schedulingHeap(events: list) -> int:
+    if events == []: return 0
+    events.sort(key = lambda event : event[0])
+    
+    heap = [events[0][::-1]]
+    for event in events[1:]:
+        prev = heapq.heappop(heap)
+        if prev[0] <= event[0]:
+            heapq.heappush(heap, event[::-1])
+        else:
+            heapq.heappush(heap, prev)
+            heapq.heappush(heap, event)
+
+    return len(heap)
+
+
+print('Should be 2:', schedulingHeap([[0, 30],[5, 10],[15, 20]]))
+print('Should be 2:', schedulingHeap([(30, 75), (0, 50), (60, 150)]))
+print('Should be 1:', schedulingHeap([[7,10],[2,4]]))
