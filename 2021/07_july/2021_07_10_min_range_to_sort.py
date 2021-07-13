d82d60d787d7d5d499f198c0df33ff4209b26c74
Cedric Stark <cedric.stark@outlook.com>
2021-07-13 20:17:43 +0200

    2021-07-10: fix for cases where minima in unsorted array decrease

--- 2021/07_july/2021_07_10_min_range_to_sort.py
+++ 2021/07_july/2021_07_10_min_range_to_sort.py
@@ -6,25 +6,29 @@ def findRange(nums):
     if length == 1: return (0, 0)
     stack = []
 
+    left, right = length, 0
+
     for n in nums:
         if stack == [] or stack[-1] <= n: stack.append(n)
         else:
-            while stack[-1] > n: stack.pop()
-            break
+            while stack != [] and stack[-1] > n: stack.pop()
+            left = min(left, len(stack))
 
-    start = len(stack)
+    if len(stack) == length: return (0, 0)
     stack = []
 
     for n in nums[::-1]:
         if stack == [] or stack[-1] >= n: stack.append(n)
         else:
-            while stack[-1] < n: stack.pop()
-            break
+            while stack != [] and stack[-1] < n: stack.pop()
+            right = max(right, length - len(stack) - 1)
 
-    return (start, length - len(stack) - 1) 
+    return (left, right) 
     
 
 print(findRange([1, 7, 9, 5, 7, 8, 10]))
 # (1, 5)
 print(findRange([1,2,4,3,6,8,2,9,9,10,11]))
 # (2, 6)
+print(findRange([1,3,5,4,2]))
+# (1, 4)
