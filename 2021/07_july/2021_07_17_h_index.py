17a4818b277ce8525375137e23b83ca7a74df0cc
Cedric Stark <cedric.stark@outlook.com>
2021-07-23 00:13:54 +0200

    2021-07-17: simplify condition in loop

--- 2021/07_july/2021_07_17_h_index.py
+++ 2021/07_july/2021_07_17_h_index.py
@@ -4,14 +4,9 @@
 def hIndex(publications):
     publications.sort(reverse = True)
 
-    if publications[0] == 0: return 0
-    if len(publications) == 1: return 1
-        
-    for h in range(1, len(publications)):
-        if publications[h - 1] >= h and publications[h] <= h:
-            return h
-            
-    return h + 1
+    for i, h in enumerate(publications):
+        if i >= h: return i
+    return len(publications)
 
 
 
