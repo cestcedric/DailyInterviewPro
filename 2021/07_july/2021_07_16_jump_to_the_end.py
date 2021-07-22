9698437d5649a7473b193eec8ca3cf2543a6cf1e
Cedric Stark <cedric.stark@outlook.com>
2021-07-22 21:24:26 +0200

    2021-07-16: add dp solutions

--- 2021/07_july/2021_07_16_jump_to_the_end.py
+++ 2021/07_july/2021_07_16_jump_to_the_end.py
@@ -3,8 +3,8 @@ from functools import lru_cache
 # O(n^2) time complexity:
 # worst case: for every entry, you can jump at best to n - 2
 # => have to check every entry before coming to base case
-# Actually O(n) time complexity, since we cache the results for each position
-# O(n) space complexity: call stack with depth at most n
+# Caching reduces time complexity on average, but still O(n^2)
+# O(n) space complexity: call stack with depth at most n, cache
 def jumpToEndRecursion(nums):
     MAX_VALUE = 10 ** 5
 
@@ -17,9 +17,45 @@ def jumpToEndRecursion(nums):
 
     return jumpRec(0)
 
-print(jumpToEnd([3, 2, 5, 1, 1, 9, 3, 4]))
+
+
+# O(n^2) time complexity: pretty much as recursive approach without caching
+# O(n) space complexity: dp array with n entries
+def jumpToEndDP(nums):
+    MAX_VALUE = 10 ** 5
+    n = len(nums)
+
+    dp = [MAX_VALUE] * n
+    dp[0] = 0
+
+    for i in range(n):
+        for j in range(i + 1, min(i + nums[i] + 1, n)):
+            dp[j] = min(dp[j], dp[i] + 1)
+
+    return dp[-1]
+
+
+# O(n^2) time complexity: same as recursive approach with caching
+# O(n) space complexity: obviously
+def jumpToEndDPeff(nums):
+    MAX_VALUE = 10 ** 5
+    n = len(nums)
+
+    dp = [MAX_VALUE] * n
+    dp[n - 1] = 0
+
+    for i in range(n - 2, -1, -1):
+        if nums[i] > 0:
+            dp[i] = 1 + min(dp[j] for j in range(i + 1, min(i + nums[i] + 1, n)))
+
+    return dp[0]
+
+
+print(jumpToEndDPeff([3, 2, 5, 1, 1, 9, 3, 4]))
 # 2
 
-print(jumpToEnd([1,2,3]))
+print(jumpToEndDPeff([1,2,3]))
 
-print(jumpToEnd([2,3,0,1,4]))
+print(jumpToEndDPeff([2,3,0,1,4]))
+
+print(jumpToEndDPeff([1,1,1,1]))
