3c297aab3af0be6f6a44aa3d6cd67d84df5a4634
Cedric Stark <cedric.stark@outlook.com>
2021-08-03 23:59:21 +0200

    2021-07-28: solve with backtracking

--- 2021/07_july/2021_07_28_permutation_of_numbers.py
+++ 2021/07_july/2021_07_28_permutation_of_numbers.py
@@ -1,6 +1,9 @@
+from collections import Counter
+
 # O(n!) time complexity: number of possible permutations
 # probably additionaly * n for list concatenation
 # O(n!) space complexity: tmp with at most n! elements, output at second to last round with (n-1)! elements
+# Easy way to adapt this solution to work for repeated elements: use tuples and set
 def permuteUnique(nums: list):
     output = [[]]
         
@@ -15,9 +18,34 @@ def permuteUnique(nums: list):
             
     return output
 
-
+# O(n!) time complexity: again additional * n included
+# O(n) space complexity: call stack depth at most n for one permutation
 def permute(nums):
-    pass
+    counter = Counter(nums)
+    output = []
+    n = len(nums)
 
-print(permute([1, 2, 3]))
+    def dfs(sequence):
+        nonlocal counter, output, n
+        if len(sequence) == n: 
+            output.append(sequence[::])
+            return
+        
+        for c in counter:
+            if counter[c] == 0: continue
+            counter[c] -= 1
+            sequence.append(c)
+            dfs(sequence)
+            sequence.pop()
+            counter[c] += 1
+
+    dfs([])
+    return output
+
+    
+
+# print(permute([1, 2, 3]))
 # [[1, 2, 3], [2, 1, 3], [2, 3, 1], [1, 3, 2], [3, 1, 2], [3, 2, 1]]
+perm = permute([2,2,1,1])
+for p in perm:
+    print(p)
