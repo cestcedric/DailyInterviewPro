3ba561cc98d66efd5e72ea40384cb816be42d4cc
Cedric Stark <cedric.stark@outlook.com>
2021-08-04 13:00:24 +0200

    2021-07-30: add approach without cumulative sum

--- 2021/07_july/2021_07_30_subarray_with_target_sum.py
+++ 2021/07_july/2021_07_30_subarray_with_target_sum.py
@@ -1,6 +1,6 @@
 # O(n) time complexity: one pass for cumulative sum, one to find edges
 # O(n) space complexity: additional cumulative sum array
-def find_continuous_k(nums, k):
+def find_continuous_k_cumsum(nums, k):
     cumSum = [0]
     for n in nums: cumSum.append(cumSum[-1] + n)
 
@@ -10,6 +10,23 @@ def find_continuous_k(nums, k):
         if intervalSum == k and left < right: return nums[left:right]
         if intervalSum > k: left += 1
         else: right += 1
+
+    return None
+
+
+# O(n) time complexity: one pass through array, look at each index at most twice
+# O(1) space complexity: work on original array
+def find_continuous_k(nums: list, k: int) -> list:
+    left, right, intervalSum = 0, 0, nums[0]
+
+    while left < len(nums) and right < len(nums):
+        if intervalSum == k and left < right: return nums[left:right + 1]
+        if intervalSum > k:
+            intervalSum -= nums[left]
+            left += 1
+        else:
+            right += 1
+            intervalSum += nums[right]
 
     return None
 
