6a69f2979df139a150464ce8b03d2489554bd436
Cedric Stark <cedric.stark@outlook.com>
2021-08-05 18:21:37 +0200

    2021-08-04: solve with iteration and by comparing level to level

---
+++ 2021/08_August/2021_08_04_binary_tree_level_with_minimum_sum.py
@@ -0,0 +1,44 @@
+class Node:
+    def __init__(self, value, left=None, right=None):
+        self.val = value
+        self.left = left
+        self.right = right
+
+
+# O(n) time: look at each node once
+# O(n) space: list with nodes in level
+def minimum_level_sum(root):
+    levelNodes, level = [root.left, root.right], 0
+    minSum, minLevel = root.val, 0
+
+    while levelNodes != []:
+        nextLevel = []
+        levelSum = 0
+        for node in levelNodes:
+            levelSum += node.val
+            if node.left is not None: nextLevel.append(node.left)
+            if node.right is not None: nextLevel.append(node.right)
+
+        if levelSum < minSum:
+            minSum, minLevel = levelSum, level
+
+        levelNodes = nextLevel
+        level += 1
+
+    return minSum
+
+
+#     10
+#    /  \
+#   2    8
+#  / \    \
+# 4   1    2
+node = Node(10)
+node.left = Node(2)
+node.right = Node(8)
+node.left.left = Node(4)
+node.left.right = Node(1)
+node.right.right = Node(2)
+
+print(minimum_level_sum(node))
+# 7
