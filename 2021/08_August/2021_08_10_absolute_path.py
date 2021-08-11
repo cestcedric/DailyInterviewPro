30c8b41de31a5354cd77306226061a79a92bf4a8
Cedric Stark <cedric.stark@outlook.com>
2021-08-11 18:42:01 +0200

    2021-08-10: reduce assumptions

--- 2021/08_August/2021_08_10_absolute_path.py
+++ 2021/08_August/2021_08_10_absolute_path.py
@@ -1,16 +1,16 @@
 # O(n) time
 # O(n) space
-# Assumes a complete path, with leading / and .. only after a named component
+# Handles .. leading to root as well as multiple ///
 def shortest_path(file_path):
     file_path = file_path.split('/')
     simplified_path = []
 
     for x in file_path:
         if x == '.': continue
-        if x == '..': simplified_path.pop()
+        if x == '..' and simplified_path != []: simplified_path.pop()
         else: simplified_path.append(x)
 
-    return '/'.join(simplified_path)
+    return '/' + '/'.join(simplified_path)
 
 print(shortest_path('/Users/Joma/Documents/../Desktop/./../'))
 # /Users/Joma/
